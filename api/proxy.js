543fded0db4eb9dffcb2507f1bff722a722597d5
Hanson-zhy <157664656+Hanson-zhy@users.noreply.github.com>
2025-02-16 11:30:36 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://accessible-dacey-hanson-zhy-1aad55ca.koyeb.app";//your website url
+  let target = "https://mega.io";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
